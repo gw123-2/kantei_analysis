4338155a37e8e7a111c60a7b0d1796211c3489c1
= <=>
2025-02-10 19:00:55 +0100

    removed start-end date calculator and saved every reshuffle as a new cabinett

--- insert_kanteidata_to_database.py
+++ insert_kanteidata_to_database.py
@@ -17,38 +17,38 @@ def setup_database(database_connection:sqlite3.Connection):
     database_cursor.execute("""
     CREATE TABLE CABINETT(
                             cabinett_number INTEGER NOT NULL,
-                            cabinett_reshuffle_times  INTEGER NOT NULL,
+                            cabinett_reshuffle  INTEGER NOT NULL,
                             start_date TEXT NOT NULL,
-                            end_date TEXT,
+                            end_date TEXT NOT NULL ,
                             prime_minister INTEGER,
 
-                            PRIMARY KEY (cabinett_number, cabinett_reshuffle_times)
+                            PRIMARY KEY (cabinett_number, cabinett_reshuffle)
                             );
 """)
     database_connection.commit()
 
 
-def scrape_cabinett_html_for_info(cabinett_site:str):
-    cabinett_number = regex.findall("第\d{1,3}代",cabinett_site)[0]
-    cabinett_shuffle = get_cabinett_shuffles(cabinett_site)
+def populate_database_with_cabinett_data(database_connection:sqlite3.Connection, cabinett_site:str):
+    cabinett_number = get_cabinett_number_from_html(cabinett_site)
+    cabinett_shuffle = get_cabinett_shuffles_from_html(cabinett_site)
+        
+    insert_new_cabinett_into_database(database_connection, cabinett_number, cabinett_shuffle)
 
-    cabinett_start_end_date = get_cabinett_start_and_end_date(cabinett_site)
+def insert_new_cabinett_into_database(database_connection:sqlite3.Connection, number, shuffles):
+    database_cursor = database_connection.cursor()
+    for i in range(len(shuffles)):
+        database_cursor.execute("""
+            INSERT INTO CABINETT (cabinett_number, cabinett_reshuffle, start_date, end_date) VALUES(?, ?, ? ,?)
+        """, (str(number), str(i), str(shuffles[i][0]), str(shuffles[i][1]))  )
+        database_connection.commit()
+    
+def get_cabinett_number_from_html(cabinett_site:str):
+    cabinett_number:str = regex.findall("第\d{1,3}代",cabinett_site)[0]
+    cabinett_number = cabinett_number.replace("第", "")
+    cabinett_number = cabinett_number.replace("代", "")
+    return cabinett_number
 
-    print(str(cabinett_start_end_date))
-
-
-
-def get_cabinett_start_and_end_date(cabinett_site):
-    raw_cabinett_dates = regex.findall(".{3,5}年\d{1,2}月\d{1,2}日<br class=\"sp-disp\">～.{3,5}年\d{1,2}月\d{1,2}日", cabinett_site)[0]
-    clean_start_end_date = clean_cabinett_start_end_date_htmltext_dates(raw_cabinett_dates)
-    return clean_start_end_date
-
-def clean_cabinett_start_end_date_htmltext_dates(raw_cabinett_dates):
-    raw_dates_only = raw_cabinett_dates.replace("<br class=\"sp-disp\">～", ";")
-    clean_start_end_date_list = raw_dates_only.split(";")
-    return clean_start_end_date_list
-
-def get_cabinett_shuffles(cabinett_site):
+def get_cabinett_shuffles_from_html(cabinett_site):
     shuffles_raw = regex.findall("<p class=\"module-detail-text module-text--note\">.{3,5}年\d{1,2}月\d{1,2}日発足　.{3,5}年\d{1,2}月\d{1,2}日現在</p>",cabinett_site )
     clean_shuffles = clean_cabinett_shuffle_htmltext_dates(shuffles_raw)
     return clean_shuffles
@@ -60,9 +60,10 @@ def clean_cabinett_shuffle_htmltext_dates(dates:list):
     return clean_dates
 
 def cleanup_shuffle_date(date:str):
-    clean_date:str = regex.findall(".{3,5}年\d{1,2}月\d{1,2}日発足　.{3,5}年\d{1,2}月\d{1,2}日現在", date)[0]
+    clean_date:str = regex.findall(">.{3,5}年\d{1,2}月\d{1,2}日発足　.{3,5}年\d{1,2}月\d{1,2}日現在", date)[0]
     clean_date = clean_date.replace("発足", "")
     clean_date = clean_date.replace("現在", "")
+    clean_date = clean_date.replace(">", "")
 
     return clean_date.split("　")
 
@@ -74,13 +75,13 @@ def get_html_text(url:str):
     return html_text
 
 if __name__ == "__main__":
-    #database_path = input("path to databse:")
-    #database_connection = sqlite3.connect(database_path)
+    database_path = input("path to databse:")
+    database_connection = sqlite3.connect(database_path)
     
-    #setup_database(database_connection)
+    setup_database(database_connection)
 
     site_to_be_analyzed = input("input url to site: ")
     cabinett_site_html = get_html_text(site_to_be_analyzed)
 
-    scrape_cabinett_html_for_info(cabinett_site_html)
+    populate_database_with_cabinett_data(database_connection, cabinett_site_html)
 
